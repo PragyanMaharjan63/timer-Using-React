6e3b2c37046dc0813cc3c0275b162f7606e66f7d
MaharjanPragyan63 <pragyanmaharjan6k@gmail.com>
2025-05-05 11:16:23 +0545

    completed timer

--- src/components/clock.jsx
+++ src/components/clock.jsx
@@ -1,34 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-export default function Clock({ inputTime }) {
+export default function Clock({ inputTime, stop }) {
   const [timeLeft, setTimeLeft] = useState(inputTime);
-  //   console.log(inputTime);
-  let stop = false;
+  const [timerStarted, setTimerStarted] = useState(false);
+  const intervalRef = useRef(null);
+
   useEffect(() => {
     setTimeLeft(inputTime);
   }, [inputTime]);
 
-  useEffect(() => {
-    if (timeLeft === 0) return;
-    const timer = setInterval(() => {
-      setTimeLeft((t) => {
-        if (t > 0 && !stop) return t - 1;
-        else return 0;
-      });
-    }, 1000);
+  function startTimer() {
+    if (!intervalRef.current) {
+      intervalRef.current = setInterval(() => {
+        setTimeLeft((t) => t - 1);
+      }, 1000);
+      setTimerStarted(true);
+    }
+  }
 
-    return () => clearInterval(timer);
-  }, [timeLeft]);
+  function stopTimer() {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTimerStarted(false);
+  }
+
+  useEffect(() => {
+    if (!stop && timeLeft > 0) {
+      startTimer();
+    } else {
+      stopTimer();
+    }
+
+    return () => stopTimer();
+  }, [stop, timeLeft]);
 
   return (
-    <>
-      <div className="text-2xl bg-amber-200 w-52 h-52 flex justify-center items-center rounded-full border-4 border-[#442312]">
-        {timeLeft === 0
-          ? "Time's up!"
-          : `${parseInt(timeLeft / 3600)}Hr ${
-              parseInt(timeLeft / 60) % 60
-            } Min ${timeLeft % 60}`}
-      </div>
-    </>
+    <div
+      className={`transition-all duration-700 ${
+        !timerStarted
+          ? "text-2xl bg-amber-200 w-52 h-52 flex justify-center items-center rounded-full border-4 border-[#442312] font-calsans text-[#442312]"
+          : "text-4xl bg-amber-200 w-72 h-72 flex justify-center items-center rounded-full border-4 border-[#442312] font-calsans text-[#442312]"
+      }`}
+    >
+      {timeLeft === 0 ? (
+        "Time's up!"
+      ) : (
+        <p>
+          {parseInt(timeLeft / 3600)}Hr {parseInt(timeLeft / 60) % 60} Min{" "}
+          {timeLeft % 60} s
+        </p>
+      )}
+    </div>
   );
 }
--- src/components/inputtime.jsx
+++ src/components/inputtime.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Clock from "./clock";
 
 export default function Timer() {
   const [Hour, setHour] = useState(0);
   const [Minutes, setMinutes] = useState(0);
   const [timer, setTimer] = useState(0);
+  const [stop, setStop] = useState(false);
+  const [isReset, setReset] = useState(false);
   const getTime = (event) => {
     event.preventDefault();
-    console.log("the hour is ", Hour);
-    console.log("the Minutes are ", Minutes);
+    // console.log("the hour is ", Hour);
+    // console.log("the Minutes are ", Minutes);
     let Totaltimer = Hour * 60 * 60 + Minutes * 60;
     setTimer(Totaltimer);
   };
+
   return (
     <div className="flex flex-col justify-center items-center space-y-3">
+      <Clock inputTime={timer} stop={stop} />;
       <form onSubmit={getTime} className="flex flex-col space-y-3">
         <div className="flex flex-row">
           <input
@@ -41,12 +45,32 @@ export default function Timer() {
           />
           <p className="text-[#100605] m-2 font-bold text-3xl">Min</p>
         </div>
-        <Clock inputTime={timer} />;
         <button
           type="submit"
-          className="bg-amber-200 border-2 border-[#442312] p-3 outline-none rounded-lg text-xl font-bold"
+          className="bg-amber-200 border-2 border-[#442312] p-3 outline-none rounded-lg text-xl text-[#442312] font-calsans"
+          onClick={
+            isReset
+              ? () => {
+                  setReset(false);
+                  setTimer(0);
+                  setMinutes(0);
+                  setHour(0);
+                }
+              : () => {
+                  getTime;
+                  setReset(true);
+                }
+          }
         >
-          Start Timer
+          {isReset ? "Reset Timer" : "Start Timer"}
+        </button>
+        <button
+          className="bg-amber-200 border-2 border-[#442312] p-3 outline-none rounded-lg text-xl text-[#442312] font-calsans"
+          onClick={() => {
+            stop ? setStop(false) : setStop(true);
+          }}
+        >
+          {stop || !isReset ? "Resume Timer" : "Pause Timer"}
         </button>
       </form>
     </div>
