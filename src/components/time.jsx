eb93b79cab8d2c44db5c73a991077ab163853088
PragyanMaharjan63 <pragyanmaharjan6k@gmail.com>
2025-05-19 09:03:26 +0545

    added blinking dots

--- src/components/time.jsx
+++ src/components/time.jsx
@@ -2,17 +2,22 @@ import { useState, useEffect } from "react";
 
 export default function time() {
   const [time, setTime] = useState(new Date());
-
+  const [dot, setDot] = useState(":");
   useEffect(() => {
     let intervaL = setInterval(() => {
       setTime(new Date()); //updates the value so it causes the interval to rerender
+      setDot((prevDot) => (prevDot === ":" ? " " : ":"));
     }, 1000);
     return () => clearInterval(intervaL); //if there is not clear interval then everytime the component mounts then it will create new interval causing multiple intervals.
   }, []);
 
   return (
     <p>
-      {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
+      {time.getHours()}
+      {dot}
+      {time.getMinutes()}
+      {dot}
+      {time.getSeconds()}
     </p>
   );
 }
